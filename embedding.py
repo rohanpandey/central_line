2f4d9ef64eb25e60ca96e3166a2cbe1cdac8fa42
rohanpandey <rohan99pandey@gmail.com>
2021-03-06 18:58:01 +0530

    changed file name

--- embedding.py
+++ embedding.py
@@ -1,48 +1,53 @@
+import os
 from gensim.scripts.glove2word2vec import glove2word2vec
 from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors
 
-Dimension=100
+def embed_column(dataframe,column,model_name,dimensions,tokens):
+  if(model_name='glove'):
+  	model=load_glove(tokens,dimensions)
+  if(model_name='biovec'):
+  	model=load_biovec(dimensions)
+  value_array=[]
+  for index,row in dataframe.iterrows():
+    note_as_vec=[]
+    preprocessed_text=apply_all(dataframe[column])
+    for word in preprocessed_text:
+      try:
+        vec_word = model[word]
+      except:
+        vec_word = [0.0]*dimensions
+      note_as_vec.append(vec_word)
+    value=np.average(note_as_vec, axis=0)
+    value_array.append(value)
+  dataframe[column+'_avg']=value_array
+  return dataframe
 
-def embed_column(dataframe,column,model):
-	value_array=[]
-		for index,row in dataframe.iterrows():
-			note_as_vec=[]
-			preprocessed_text=apply_all(dataframe[column])
-			for word in preprocessed_text:
-				try:
-				    vec_word = model[word]
-				except:
-					vec_word = [0.0]*Dimension
-        		note_as_vec.append(biowordvec_word)
-        	value=np.average(note_as_vec, axis=0)
-        	value_array.append(value)
-	dataframe[column+'_avg']=value_array
-	return dataframe
+def load_biovec(dimensions)
+	file_path="/BioWordVec_d{}.vec.bin".format(dimensions)
+	biovecpath = os.path.expanduser(file_path)
+	biovec = gensim.models.KeyedVectors.load_word2vec_format(biovecpath, binary=True)
+	return biovec
 
-def load_glove()
-	Word2vecpath = os.path.expanduser("insert location of downloaded file here") 
-	word2vec = gensim.models.KeyedVectors.load_word2vec_format(word2vecpath, binary=True)
-	return word2vec
-
-def load_w2v()
-	glovepath = os.path.expanduser("insert location of downloaded file here")
-	temp_file = "/tmp/glove.840B.300d.w2v.txt"
-	glove2word2vec(glovepath, temp_file)
-	glove = gensim.models.KeyedVectors.load_word2vec_format(tmp_file)
-	return glove
+def load_glove(tokens,dimensions):
+  file_path="/glove.{}B.{}d.txt".format(tokens,dimensions)
+  glovepath = os.path.expanduser(file_path)
+  temp_file = "/tmp/glove.840B.300d.w2v.txt"
+  glove2word2vec(glovepath, temp_file)
+  glove = KeyedVectors.load_word2vec_format(temp_file)
+  return glove
 
 def avg_weighting(dataframe,column):
-	val=0
-	for index,rows in dataframe.iterrows():
-		val+=dataframe[column]
-	val=val/len(DataFrame.index) 
-	return val
+  val=0
+  for index,row in dataframe.iterrows():
+    val+=row[column]
+  val=val/len(dataframe.index)
+  return val
 
 def exp_weighting(dataframe,column,date_column,procedure_date,gap):
 	val=0
-	for index,rows in dataframe.iterrows():
-		delta=procedure_date-dataframe[date_column]-7
-		val+=exp(-(delta**2))*dataframe[column]
-	val=val/len(DataFrame.index) 
+	for index,row in dataframe.iterrows():
+		delta=procedure_date-row[date_column]-gap
+		val+=exp(-(delta**2))*row[column]
+	val=val/len(dataframe.index) 
 	return val
---
+++ word_embedding.py
@@ -0,0 +1,48 @@
+from gensim.scripts.glove2word2vec import glove2word2vec
+from gensim.test.utils import datapath, get_tmpfile
+from gensim.models import KeyedVectors
+
+Dimension=100
+
+def embed_column(dataframe,column,model):
+	value_array=[]
+		for index,row in dataframe.iterrows():
+			note_as_vec=[]
+			preprocessed_text=apply_all(dataframe[column])
+			for word in preprocessed_text:
+				try:
+				    vec_word = model[word]
+				except:
+					vec_word = [0.0]*Dimension
+        		note_as_vec.append(biowordvec_word)
+        	value=np.average(note_as_vec, axis=0)
+        	value_array.append(value)
+	dataframe[column+'_avg']=value_array
+	return dataframe
+
+def load_glove()
+	Word2vecpath = os.path.expanduser("insert location of downloaded file here") 
+	word2vec = gensim.models.KeyedVectors.load_word2vec_format(word2vecpath, binary=True)
+	return word2vec
+
+def load_w2v()
+	glovepath = os.path.expanduser("insert location of downloaded file here")
+	temp_file = "/tmp/glove.840B.300d.w2v.txt"
+	glove2word2vec(glovepath, temp_file)
+	glove = gensim.models.KeyedVectors.load_word2vec_format(tmp_file)
+	return glove
+
+def avg_weighting(dataframe,column):
+	val=0
+	for index,rows in dataframe.iterrows():
+		val+=dataframe[column]
+	val=val/len(DataFrame.index) 
+	return val
+
+def exp_weighting(dataframe,column,date_column,procedure_date,gap):
+	val=0
+	for index,rows in dataframe.iterrows():
+		delta=procedure_date-dataframe[date_column]-7
+		val+=exp(-(delta**2))*dataframe[column]
+	val=val/len(DataFrame.index) 
+	return val
