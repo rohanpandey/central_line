61b31f32e924e6aef5b36bc22217a04983973185
rohanpandey <rohan99pandey@gmail.com>
2021-03-07 09:34:00 +0530

    Update preprocess_text.py

--- preprocess_text.py
+++ preprocess_text.py
@@ -8,7 +8,13 @@ from nltk.stem.porter import PorterStemmer
 from nltk import word_tokenize
 import re
 
+
 def initial_clean(text):
+	'''
+	Function to remove extra characters, convert to lowercase, and tokenize the sentence.
+	Input: String
+	Output: list of words
+	'''
     text = re.sub("((\S+)?(http(s)?)(\S+))|((\S+)?(www)(\S+))|((\S+)?(\@)(\S+)?)", " ", text)
     text = re.sub("[^a-zA-Z ]", "", text)
     text = text.lower()
@@ -16,6 +22,11 @@ def initial_clean(text):
     return text
 
 def pad_punctuation(text):
+	'''
+	Function to add extra spaces around punctuations for improved tokenization
+	Input: String 
+	Output: String
+	'''
     PUNCTS = "!\"#$%'()*+,-./:;<=>?@[\]^_`{|}~"
     updated_list = []
     for i in range(len(text)):
@@ -26,6 +37,11 @@ def pad_punctuation(text):
     return ''.join(updated_list)
 
 def stem_words(text):
+	'''
+	Function for stemming words.
+	Input: List of words
+	Output: List of words
+	'''
   stemmer = PorterStemmer()
   try:
     text = [stemmer.stem(word) for word in text]
@@ -35,6 +51,11 @@ def stem_words(text):
   return text
 
 def lem_words(text):
+	'''
+	Function for lemmatizing words.
+	Input: List of words
+	Output: List of words
+	'''
   lemmatizer = WordNetLemmatizer()
   try:
     text = [lemmatizer.lemmatize(word) for word in text]
@@ -44,9 +65,19 @@ def lem_words(text):
   return text
 
 def remove_stop_words(text):
+	'''
+	Function for removing stopwords words.
+	Input: List of words
+	Output: List of words
+	'''
   stop_words = stopwords.words('english')
   return [word for word in text if word not in stop_words]
 
 def apply_all(text):
+	'''
+	Function to apply all preprocessing techniques.
+	Input: string
+	Output: List of words
+	'''
   summary = lem_words(stem_words(remove_stop_words(initial_clean(pad_punctuation(text)))))
   return summary
